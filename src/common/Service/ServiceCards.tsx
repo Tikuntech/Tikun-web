dea8cf2000c8c45f1b52bbfbbb6fd6278172516e
BestCoder <58046289+MarotiKathoke@users.noreply.github.com>
2024-09-07 23:18:34 +0530

    service page changes

--- src/common/Service/ServiceCards.tsx
+++ src/common/Service/ServiceCards.tsx
@@ -1,6 +1,15 @@
 import { ServiceHoverEffect } from "@/components/ui/CardHoverService";
-import { HoverEffect } from "../../components/ui/card-hover-effect";
-import GlobeIcon from '../../../public/Service/Web Icon.svg'
+import  GlobeIcon  from '../../../public/Service/Web Icon.svg'
+import MobileIcon from '../../../public/Service/mobile.svg';
+import SecurityIcon from '../../../public/Service/Security Icon.svg';
+import MarketingIcon from '../../../public/Service/digitalMarketing.svg';
+import CloudIcon from '../../../public/Service/Cloud Icon.svg';
+import SoftwareIcon from '../../../public/Service/Coding Icon (1).svg';
+import ManagementIcon from '../../../public/Service/Planner Icon.svg';
+import DataAnalysisIcon from '../../../public/Service/Analysis Icon.svg';
+import NetworkIcon from '../../../public/Service/Network Icon.svg';
+import { IoGlobeOutline } from "react-icons/io5";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export function ServiceCards() {
   return (
@@ -12,55 +21,100 @@ export function ServiceCards() {
 
 export const services = [
   {
-    icon: <GlobeIcon/>,
+    icon: (
+      <div className="flex justify-between  w-full">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Web Development",
     description: "Create websites that are both visually appealing and functional for users.",
     link: "/web-development",
   },
   {
-    icon: "mobile",
+    icon: (
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Mobile Development",
     description: "Create or enhance mobile applications, ensuring optimized user experience.",
     link: "/mobile-development",
   },
   {
-    icon: "shield-check",
+    icon:(
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Cyber Security",
     description: "Provides advanced protection for your data and systems against threats.",
     link: "/cyber-security",
   },
   {
-    icon: "megaphone",
+    icon: (
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Digital Marketing",
     description: "Create solutions to help businesses achieve their digital marketing goals.",
     link: "/digital-marketing",
   },
   {
-    icon: "cloud",
+    icon:(
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Cloud Computing",
     description: "Provides access to scalable computing resources to optimize their operations.",
     link: "/cloud-computing",
   },
   {
-    icon: "code",
+    icon:(
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Software Development",
     description: "Provides customized solutions to create and maintain software applications.",
     link: "/software-development",
   },
   {
-    icon: "rocket-launch",
+    icon: (
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "IT Project Management",
     description: "Provides businesses with expert guidance and support for IT projects.",
     link: "/it-project-management",
   },
   {
-    icon: "magnifying-glass",
+    icon: (
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Data Analysis",
     description: "Provide data analysis for varied categories to help client businesses.",
     link: "/data-analysis",
   },
   {
-    icon: "network",
+    icon: (
+      <div className="flex justify-between space-x-2">
+        <IoGlobeOutline style={{ height: 60, width: 60 }} />
+        <FaArrowRightLong style={{ height: 60, width: 60 }} />
+      </div>
+    ),
     title: "Network Management",
     description: "Provide management of network infrastructure design for clients.",
     link: "/network-management",
--- src/components/ui/CardHoverService.tsx
+++ src/components/ui/CardHoverService.tsx
@@ -1,63 +1,74 @@
-"use client"
+"use client";
 import { cn } from "@/lib/utils";
-import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
 import { useState } from "react";
+import Link from "next/link";
+import { FiSlack } from "react-icons/fi";
+
+
 
 export const ServiceHoverEffect = ({
   items,
   className,
 }: {
   items: {
-    title: string;
-    description: string;
-    link: string;
+      icon: JSX.Element;
+      title: string;
+      description: string;
+      link: string;
   }[];
   className?: string;
 }) => {
-  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
-    <div
-      className={cn(
-        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10",
-        className
-      )}
-    >
-      {items.map((item, idx) => (
-        <Link
-          href={item?.link}
-          key={item?.link}
-          className="relative group block p-2 h-full w-full"
-          onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          {/* <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence> */}
-          <Card>
-            <CardTitle>{item.title}</CardTitle>
-            <CardDescription>{item.description}</CardDescription>
-          </Card>
-        </Link>
-      ))}
-    </div>
+      <div
+          className={cn(
+              "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10",
+              className
+          )}
+      >
+          {items.map((item, idx) => (
+              <Link
+                  href={item.link}
+                  key={item.link}
+                  className="relative group block p-2 h-full w-full"
+                  onMouseEnter={() => setHoveredIndex(idx)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+              >
+                  {hoveredIndex === idx ? (
+                      <Card>
+                          <h1 className="text-black font-normal mt-4 text-center">C Y B E R   S E C U R I T Y.</h1>
+                          <FiSlack className="text-white text-center block mx-auto mt-4" />
+                          <h1 className="text-white font-bold mt-4 text-center text-3xl">Mika</h1>
+                          <h1 className="text-white font-bold text-center text-3xl">Medika</h1>
+                          <p className="text-xs text-center mt-10">
+                              Mika Medika Healthcare, a large hospital network, was concerned about the security of their patient data.
+                          </p>
+                          <div className="flex justify-center mt-6">
+                              <button className="bg-black text-white p-2 rounded-md">
+                                  READ MORE
+                              </button>
+                          </div>
+                      </Card>
+                  ) : (
+                      <Card>
+                          <CardIcon className="mb-4">
+                              {item.icon}
+                          </CardIcon>
+                          <div className="h-80 flex flex-col justify-end p-4">
+                              <div className="text-black font-normal text-left">
+                                  <CardTitle>{item.title}</CardTitle>
+                                  <CardDescription>{item.description}</CardDescription>
+                              </div>
+                          </div>
+                      </Card>
+                  )}
+              </Link>
+          ))}
+      </div>
   );
 };
+
 
 export const Card = ({
   className,
@@ -66,49 +77,64 @@ export const Card = ({
   className?: string;
   children: React.ReactNode;
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div
-      className={cn(
-        "rounded-2xl h-80 w-96 p-4 overflow-hidden bg-gradient-to-b from-gray-500 to-slate-800 border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}
-    >
-      <div className="relative z-50">
-        <div className="p-4">{children}</div>
+      <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={cn(
+              isHovered
+                  ? "text-white px-4 py-2 rounded-md h-full hover:bg-custom-blue transition-colors duration-300 hover:shadow-lg hover:scale-105 hover:rotate-1 cursor-pointer"
+                  : "bg-gradient-to-b from-slate-400 to-slate-800 text-white px-4 py-2 rounded-md hover:bg-custom-blue transition-colors duration-300 hover:shadow-lg hover:scale-105 hover:rotate-1 cursor-pointer",
+              className
+          )}
+      >
+          <div className="relative z-50">
+              <div className="p-4">{children}</div>
+          </div>
       </div>
-    </div>
   );
 };
 
 export const CardTitle = ({
-  className,
-  children,
+    className,
+    children,
 }: {
-  className?: string;
-  children: React.ReactNode;
+    className?: string;
+    children: React.ReactNode;
 }) => {
-  return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
-      {children}
-    </h4>
-  );
+    return (
+        <h4 className={cn("text-white text-3xl font-bold tracking-wide", className)}>
+            {children}
+        </h4>
+    );
 };
 
 export const CardDescription = ({
-  className,
-  children,
+    className,
+    children,
 }: {
-  className?: string;
-  children: React.ReactNode;
+    className?: string;
+    children: React.ReactNode;
 }) => {
-  return (
-    <p
-      className={cn(
-        "mt-10 text-white tracking-wide leading-relaxed text-sm",
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
+    return (
+        <p className={cn("text-white tracking-wide leading-relaxed ", className)}>
+            {children}
+        </p>
+    );
+};
+
+export const CardIcon = ({
+    className,
+    children,
+}: {
+    className?: string;
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className={`text-white   ${className}`}>
+            {children}
+        </div>
+    );
 };
