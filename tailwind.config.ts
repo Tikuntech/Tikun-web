397aae2b9c41248edf5daba9e4e0241a9be510be
BestCoder <58046289+MarotiKathoke@users.noreply.github.com>
2024-09-10 21:43:40 +0530

    final changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,15 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/common/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/Home/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/home/**/*.{js,ts,jsx,tsx,mdx}',
     "./src/**/*.{js,ts,jsx,tsx}",
     './src/app/page.tsx',
-    './src/app/**/*.{js,ts,jsx,tsx}'
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
     
   ],
  
